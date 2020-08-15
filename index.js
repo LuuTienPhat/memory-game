872974ce0e6e3f10bd13e334955afa65cd9f0da5
Phat <you@example.com>
2020-08-15 17:49:10 +0700

    delete LocalStorage

--- index.js
+++ index.js
@@ -8,24 +8,21 @@ const showTime = document.querySelector("#show-time");
 const table = document.getElementById("table");
 const restartButton = document.getElementById("restart-btn");
 
-let interval;
+let interval; 
 let count;
 let currentData;
 let historyResults = [];
+
+
+startButton.addEventListener("click", startGame);
 
 restartButton.addEventListener("click", function () {
   window.location.reload();
 });
 
 function startGame() {
-  // shuffle();
   hideWelcomebox();
-  let second = 0;
-  interval = setInterval(() => {
-    second += 1;
-    count = second;
-    // console.log(second);
-  }, 1000);
+  counter();
 }
 
 function hideWelcomebox() {
@@ -33,67 +30,10 @@ function hideWelcomebox() {
   welcomeBox.style.display = "none";
 }
 
-startButton.addEventListener("click", startGame);
 
-function stopCount() {
-  clearInterval(interval);
-  // console.log("Time:", count);
-}
 
-function handleTableData(arr) {
-  arr.sort((a, b) => {
-    return a.time - b.time;
-  });
-  historyResults = [...historyResults.splice(0, 3)];
-  return historyResults;
-}
 
-function renderHistoryTable() {
-  handleTableData(historyResults);
-  let content = historyResults.map((result, index) => {
-    return `<tr class="${currentData === result ? "active" : ""}">
-        <th scope="row">${index + 1}</th>
-        <td>${result.date}</td>
-        <td>${result.time}s</td>
-      </tr>`;
-  });
-  table.innerHTML = `<tbody>${content.join("")}</tbody>`;
-}
 
-function checkTime(num) {
-  return num < 10 ? "0" + num : num;
-}
-
-function saveDataGame() {
-  let d = new Date();
-
-  const day = checkTime(d.getDate());
-  const month = checkTime(d.getMonth() + 1);
-  const year = d.getFullYear();
-  const hour = checkTime(d.getHours());
-  const minute = checkTime(d.getMinutes());
-  const data = `${day}/${month}/${year} ${hour}:${minute}`;
-
-  currentData = { date: data, time: count };
-
-  historyResults.push(currentData);
-}
-
-function loadfromLocalStorage() {
-  if (!localStorage.history) return;
-
-  historyResults = JSON.parse(localStorage.getItem("history"));
-}
-
-function storeToLocalStorage() {
-  const dataString = JSON.stringify(historyResults);
-  localStorage.setItem("history", dataString);
-}
-
-// function disableOverlay() {
-//   overlay.style.display = "none";
-//   console.log("hello");
-// }
 function showSummaryBox() {
   setTimeout(() => {
     stopCount();
@@ -114,4 +54,75 @@ function displayTime() {
   showTime.innerHTML = `Your time: ${count}s`;
 }
 
+
+
+
+
+function handleTableData(historyResults) {
+  historyResults.sort((a, b) => {
+    return a.time - b.time;
+  });
+  historyResults = [...historyResults.splice(0, 3)];
+  return historyResults;
+}
+
+function renderHistoryTable() {
+  handleTableData(historyResults);
+  let content = historyResults.map((result, index) => {
+    return `<tr class="${currentData === result ? "active" : ""}">
+          <th scope="row">${index + 1}</th>
+          <td>${result.date}</td>
+          <td>${result.time}s</td>
+        </tr>`;
+  });
+  table.innerHTML = `<tbody>${content.join("")}</tbody>`;
+}
+
+function checkTime(num) {
+  return num < 10 ? "0" + num : num;
+}
+
+function saveDataGame() {
+  let d = new Date();
+
+  const day = checkTime(d.getDate());
+  const month = checkTime(d.getMonth() + 1);
+  const year = d.getFullYear();
+  const hour = checkTime(d.getHours());
+  const minute = checkTime(d.getMinutes());
+  const data = `${day}/${month}/${year} ${hour}:${minute}`;
+
+  currentData = {
+    date: data,
+    time: count
+  };
+
+  historyResults.push(currentData);
+}
+
+function loadfromLocalStorage() {
+  if (!localStorage.history) return;
+
+  historyResults = JSON.parse(localStorage.getItem("history"));
+}
+
+function storeToLocalStorage() {
+  const dataString = JSON.stringify(historyResults);
+  localStorage.setItem("history", dataString);
+}
+
 export default showSummaryBox;
+
+
+
+function counter(){
+    let second = 0;
+    interval = setInterval(() => {
+      second += 1;
+      count = second;
+    }, 1000);
+  }
+
+function stopCount() {
+    clearInterval(interval);
+  }
